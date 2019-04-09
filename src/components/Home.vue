087a3ecf405a591805c18be31433d8c63bc81008
SencerH <sencerhamarat@gmail.com>
2019-04-09 23:55:33 +0300

    Initial Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+            <span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
+        </div>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        props: {
+            msg: String
+        },
+        computed: {
+            isLoggedIn: function () {
+                return this.$store.getters.isLoggedIn
+            }
+        },
+        methods: {
+            logout: function () {
+                this.$store.dispatch('logout')
+                    .then(() => {
+                        this.$router.push('/login')
+                    })
+            }
+        },
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import Axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$http = Axios;
+
+const accessToken = localStorage.getItem('access_token');
+
+if (accessToken) {
+    Vue.prototype.$http.defaults.headers.common['Authorization'] = accessToken
+}
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
